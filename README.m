d331970c16f9d3a6603a59d5686f39b738eccabe
MiguelAQuevedoP <mquevedo@unicauca.edu.co>
2023-10-07 16:44:52 -0500

    add README.m

---
+++ README.m
@@ -0,0 +1,34 @@
+# 🌟 Ejercicios de Diseño Web Responsivo de freeCodeCamp 🌟
+
+¡Bienvenido a mi repositorio de ejercicios resueltos del emocionante curso de Diseño Web Responsivo de freeCodeCamp! Aquí encontrarás mis soluciones a los desafíos planteados en el curso.
+
+## ¿Qué encontrarás aquí?
+
+En este repositorio, he compartido mis implementaciones de los ejercicios del curso, personalizándolos a mi estilo. Mi objetivo es mostrar cómo puedo aplicar los principios del diseño web responsivo.
+
+## ¿Cómo puedes usar este repositorio?
+
+Si estás estudiando diseño web responsivo o simplemente buscas inspiración, este repositorio util para ti. Puedes explorar mis soluciones, aprender de ellas o incluso utilizarlas como punto de partida para tus propios proyectos.
+
+## Cómo navegar por el repositorio
+
+- 📁 Cada ejercicio tiene su propia carpeta, nombrada de manera descriptiva y organizada de manera lógica.
+- 🚀 Dentro de cada carpeta, encontrarás los archivos HTML, CSS y, si es necesario, archivos JavaScript asociados al ejercicio.
+- 📝 He incluido comentarios en el código para explicar mi enfoque y las decisiones de diseño.
+- 💡 Si tienes preguntas o sugerencias, ¡no dudes en abrir una Issue o una solicitud de extracción (Pull Request)!
+
+## ¿Cómo contribuir?
+
+¡Me encantaría recibir tus contribuciones! Si tienes soluciones alternativas, mejoras o correcciones, ¡estaré encantado de revisarlas y considerarlas! Simplemente sigue estos pasos:
+
+1. Haz un Fork (bifurcación) de este repositorio.
+2. Realiza tus cambios en tu propio repositorio Fork.
+3. Envía una solicitud de extracción (Pull Request) para que pueda revisar tus contribuciones.
+
+## Agradecimientos
+
+Agradezco a freeCodeCamp por ofrecer este increíble curso que me ha permitido aprender y mejorar mis habilidades de diseño web responsivo. Este repositorio es mi manera de devolver a la comunidad.
+
+¡Espero que disfrutes explorando mis soluciones y que te inspiren en tu viaje de diseño web responsivo! Si tienes alguna pregunta o comentario, ¡no dudes en ponerte en contacto conmigo.
+
+¡Diviértete codificando! 🚀
